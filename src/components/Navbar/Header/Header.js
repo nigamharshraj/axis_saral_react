7121b037e901b71b666d35792e92bd4d175fb05b
Harsh Nigam <nigamharshraj@gmail.com>
2023-01-19 17:30:04 +0530

    react nav bar

---
+++ src/components/Navbar/Header/Header.js
@@ -0,0 +1,23 @@
+
+import "./Header.css";
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+      <header>
+        <h1 className="header-wrapper">AXIS BANK</h1>
+      </header>
+      </div>
+    );
+  }
+  export default Header;
+
+  
+
+  
+
+
+
+
+  
